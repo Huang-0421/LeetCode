eaa5f078b6a9bc16e8d0897b242953842097fb64
Huang-0421 <r1271231915@outlook.com>
2024-05-10 00:38:34 +0800

    No1

---
+++ Solution.java
@@ -0,0 +1,21 @@
+package LeetCode;
+
+public class Solution {
+    public int search(int[] nums, int target) {
+        int i = 0;
+        int j = nums.length - 1;
+        int avg;
+        while(i <= j){
+            avg = (i + j) / 2;
+            if(nums[avg] == target)
+                return avg;
+            else if(nums[avg] < target){
+                i = avg + 1;
+            }
+            else
+                j = avg - 1;
+        }
+        return -1;
+    }
+}
+
